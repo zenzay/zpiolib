099fe57a3d1ba48da8682c5f0bcc29c64c316c58
Jens Hansen <31970501+zenzay@users.noreply.github.com>
2023-08-26 20:13:11 +0200

    Add files via upload

---
+++ tests/binary-led.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Zenzay's RPi Input/Output Library.
+
+    Binary-LED Example - Blinking a Binary LED
+
+    Created by Jens Hansen 2019.
+
+"""
+
+import sys
+import time
+import pigpio
+
+from zpiolib.led.binary_led import Binary_LED
+
+LED_PIN = 20
+
+if __name__ == '__main__':
+
+    pi = pigpio.pi()
+    if not pi.connected:
+        print("Unable to connect to pigpio daemon!")
+        exit(0)
+    
+    led = Binary_LED(pi, LED_PIN)
+    print("Blinking LED. Ctrl-C to stop.")
+    try:
+        while True:
+            led.toggle()
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print(" - Bye!")
+    finally:
+        led.turn_off()
+        pi.stop()
